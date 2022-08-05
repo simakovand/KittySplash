1884e667e3ba18e51fc321873a23097cd0ddb5b4
Simakov <simakovand9@gmail.com>
2022-08-05 16:20:51 +0300

    asd

--- client/src/config/endPoints.js
+++ client/src/config/endPoints.js
@@ -19,3 +19,36 @@ export const getAllSkins = () => `${host()}/skins`;
 export const postSkin = () => `${host()}/skins`;
 export const getUserSkins = (id) => `${host()}/skins/user/${id}`;
 export const putSkinUser = () => `${host()}/skins/user`;
+
+skins/pipo-nekonin0021.png
+skins/pipo-nekonin031.png
+skins/pipo-nekonin0033.png
+skins/pipo-nekonin0044.png
+skins/pipo-nekonin0055.png
+skins/pipo-nekonin0066.png
+skins/pipo-nekonin0077.png
+skins/pipo-nekonin0088.png
+skins/pipo-nekonin0099.png
+skins/pipo-nekonin0101.png
+skins/pipo-nekonin0111.png
+skins/pipo-nekonin00111.png
+skins/pipo-nekonin0121.png
+skins/pipo-nekonin0131.png
+skins/pipo-nekonin0141.png
+skins/pipo-nekonin0151.png
+skins/pipo-nekonin0161.png
+skins/pipo-nekonin0171.png
+skins/pipo-nekonin0181.png
+skins/pipo-nekonin0191.png
+skins/pipo-nekonin0201.png
+skins/pipo-nekonin0211.png
+skins/pipo-nekonin0221.png
+skins/pipo-nekonin0231.png
+skins/pipo-nekonin0241.png
+skins/pipo-nekonin0251.png
+skins/pipo-nekonin0261.png
+skins/pipo-nekonin0271.png
+skins/pipo-nekonin0281.png
+skins/pipo-nekonin0291.png
+skins/pipo-nekonin0301.png
+
